0abfb11e4c1b5e083c7638cb484b6abaf7a591c4
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-18 17:51:27 +0530

    Selenium Setup,Locating Web Elements, Timeouts

--- Session-1/Timeouts.java
+++ Session-1/Timeouts.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Timeouts {
 
@@ -16,25 +19,32 @@ public class Timeouts {
 		//Step:2 initialisizng webdriver
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		//step:3 give base url
-		driver.get("http://www.facebook.com");
-		
-		
+		 
 		//implicit wait timeout
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		
-		
 		//scripted timeout
-		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+		 driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+		
+		//The pageLoadTimeout is the method used to set the time for 
+		//the entire page load prior to throwing an exception. 
+		//If the timeout time is set to negative, then the time taken 
+		//to load the page is endless.
 		
 		//page loadtimeout
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		
+		 driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		 
+		 
+		driver.get("http://www.facebook.com");
+				
+		//explicit wait
+		WebDriverWait explicitwait= new WebDriverWait(driver, 10);
+		explicitwait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("email")));
+	   
 		WebElement email= driver.findElement(By.id("email"));
-		email.sendKeys("teat@xyz.com");
+	    email.sendKeys("teat@xyz.com");
 		
-		//driver.close();
+		driver.close();
 		
 	}
 }
